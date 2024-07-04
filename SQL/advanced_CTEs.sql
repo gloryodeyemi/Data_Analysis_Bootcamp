7d94d8654b5acf98da80c1c1826d9917bbff3af5
Glory Odeyemi <gloryodeyemi.nike@gmail.com>
2024-07-04 17:11:31 -0400

    CTEs

---
+++ SQL/advanced_CTEs.sql
@@ -0,0 +1,47 @@
+SELECT *
+FROM employee_demographics;
+SELECT *
+FROM employee_salary;
+SELECT *
+FROM parks_departments;
+
+-- CTEs (readability and advanced calculations) -> It is temporary
+WITH CTE_Example AS
+(
+	SELECT gender, AVG(salary) avg_sal, MAX(salary) max_sal, MIN(salary) min_sal, COUNT(salary) count_sal
+    FROM employee_demographics dem
+    JOIN employee_salary sal
+		ON dem.employee_id = sal.employee_id
+	GROUP BY gender
+)
+SELECT AVG(avg_sal)
+FROM CTE_Example;
+
+WITH CTE_Example AS
+(
+	SELECT employee_id, gender, birth_date
+    FROM employee_demographics 
+    WHERE birth_date > '1985-01-01'
+),
+CTE_Example2 AS
+(
+	SELECT employee_id, salary
+    FROM employee_salary
+    WHERE salary > 50000
+)
+SELECT *
+FROM CTE_Example
+JOIN CTE_Example2
+	ON CTE_Example.employee_id = CTE_Example2.employee_id;
+    
+WITH CTE_Example (Gender, AVG_Sal, MAX_Sal, MIN_Sal, COUNT_Sal) AS
+(
+	SELECT gender, AVG(salary) avg_sal, MAX(salary) max_sal, MIN(salary) min_sal, COUNT(salary) count_sal
+    FROM employee_demographics dem
+    JOIN employee_salary sal
+		ON dem.employee_id = sal.employee_id
+	GROUP BY gender
+)
+SELECT *
+FROM CTE_Example;
+
