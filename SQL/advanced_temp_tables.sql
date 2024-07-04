e59828169670c3172b739083f3840befe384996d
Glory Odeyemi <gloryodeyemi.nike@gmail.com>
2024-07-04 17:35:10 -0400

    Temp tables

---
+++ SQL/advanced_temp_tables.sql
@@ -0,0 +1,25 @@
+-- Temporary Tables -> only visible in the session they are created in
+
+CREATE TEMPORARY TABLE temp_table
+(
+	first_name varchar(50),
+    last_name varchar(50),
+    favorite_movie varchar(100)
+);
+
+SELECT *
+FROM temp_table;
+
+INSERT INTO temp_table
+VALUES('Glory', 'Odeyemi', 'The Big Bang Theory');
+
+SELECT *
+FROM employee_salary;
+
+CREATE TEMPORARY TABLE salary_over_50k
+SELECT *
+FROM employee_salary
+WHERE salary >= 50000;
+
+SELECT *
+FROM salary_over_50k;
