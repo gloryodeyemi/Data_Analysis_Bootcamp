dc1f025723d72c7593d0c31e3694b5837fef2a01
Glory Odeyemi <gloryodeyemi.nike@gmail.com>
2024-07-11 19:14:21 -0400

    Data cleaning project

--- SQL/Project.sql
+++ SQL/Project.sql
@@ -124,17 +124,28 @@ AND t2.industry IS NOT NULL;
 
 UPDATE layoffs_staging2
 SET industry = NULL
-WHERE industry = '';
+WHERE industry = ''; -- update blank industry to null
 
 UPDATE layoffs_staging2 t1
 JOIN layoffs_staging2 t2
 	ON t1.company = t2.company
 SET t1.industry = t2.industry
 WHERE (t1.industry IS NULL OR t1.industry = '')
-AND t2.industry IS NOT NULL;
+AND t2.industry IS NOT NULL; -- update the industry column 
 
+-- Remove unnecessary columns or rows
+SELECT *
+FROM layoffs_staging2
+WHERE total_laid_off IS NULL
+AND percentage_laid_off IS NULL;
 
+DELETE
+FROM layoffs_staging2
+WHERE total_laid_off IS NULL
+AND percentage_laid_off IS NULL;
 
+ALTER TABLE layoffs_staging2
+DROP COLUMN row_num;
 
 
 
